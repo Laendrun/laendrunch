670f0b9750aa0a8f68d4b21ca621487e2a082496
laendrun <aeby281@gmail.com>
2020-06-13 17:34:48 +0200

    :bulb: Base API

---
+++ server/index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res, next) => {
+    res.json({
+        message: '🛠 Hello World 🛠'
+    });
+});
+
+function notFound(req, res, next) {
+  res.status(404);
+  const error = new Error('Not Found - ' + req.originalUrl);
+  next(error);
+}
+
+function errorHandler(err, req, res, next) {
+  res.status(res.statusCode || 500);
+  res.json({
+    message: err.message,
+    stack: err.stack
+  });
+}
+
+app.use(notFound);
+app.use(errorHandler);
+
+const port = process.env.PORT || 4040; 
+app.listen(port, () => {
+    console.log(`App listening on port ${port}!`)
+});
