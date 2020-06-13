54a2e2edb9150b0437e9cdfc656e1f31c5142f65
laendrun <aeby281@gmail.com>
2020-06-13 20:17:06 +0200

    :fire: Users can now login with the API

--- server/api/routes/auth.js
+++ server/api/routes/auth.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const AuthController = require('../controllers/auth');
 
 router.post('/signup', AuthController.auth_signup);
+router.post('/login', AuthController.auth_login);
 
 module.exports = router;
